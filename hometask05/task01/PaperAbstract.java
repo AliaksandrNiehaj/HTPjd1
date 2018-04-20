709acba9fb2415d07d3e46f2d22851699b842452
AliaksandrNiehaj <34266753+AliaksandrNiehaj@users.noreply.github.com>
2018-04-20 08:45:14 +0300

    Delete PaperAbstract.java

--- hometask05/task01/PaperAbstract.java
+++
@@ -1,9 +0,0 @@
-package by.htp.jd1.unit5.hometask05.task01;
-
-public abstract class PaperAbstract extends StationeryAbstract {
-	
-	public void paperProdNaim() {
-		System.out.println("Paper Production");
-	}
-	public abstract void forWhat();
-}
